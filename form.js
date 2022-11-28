21c63fa0654f830e2bb80aace2bf281ceaeb2524
tsohleDev <mokhemisitsohle@gmail.com>
2022-11-28 17:01:39 +0200

    Add books dynamically

--- form.js
+++ form.js
@@ -1,55 +1,62 @@
-// dom elements
 const Form = document.getElementById('booksform');
 const Bookscontainer = document.querySelector('.books');
 const bookname = Form['title'];
 const writer = Form['name'];
 
-const books = [
-   
-];
+let title ='' 
+let nam = ''
 
-let book = function Book(title, name) {
+const books = [];
+
+function Book(title, name) {
     this.title = title;
     this.name = name;
 }
 
-const addBook = (title , name) => {
-    books.push({
-        title,
-        name,
-    });
-
-    return {title, name};
+const addBook = (book) => {
+    books.push(book);
+    createBooksElements(book)
 };
 
+bookname.addEventListener('input', () => {
+    title = bookname.value
+})
+
+writer.addEventListener('input', () => {
+    nam = writer.value
+})
 
 
 const createBooksElements = ({title , name})=> {
     const Booksdiv = document.createElement ('div');
     const BookTitle = document.createElement('p');
     const BookWritter = document.createElement('p');
+    const remove = document.createElement('button')
+    remove.addEventListener('click', () => {
+        books.forEach(book => {
+            if (book.title === title && book.name === name) {
+                let i = books.indexOf(book)
+
+                books.splice(i,1)
+            }
+        })
+    })
+    const line = document.createElement('hr')
 
     BookTitle.innerText = " Title: " + title;
     BookWritter.innerText = " Writer: " + name;
 
     Booksdiv.append(BookTitle, BookWritter);
     Bookscontainer.appendChild(Booksdiv);
+    Bookscontainer.appendChild(remove)
+    Bookscontainer.appendChild(line)
 }
 
 books.forEach(createBooksElements)
 
 Form.onsubmit = (e) => {
     e.preventDefault();
-    
-    // const newBook = addBook(
-    //     titleInput.value,
-    //     nameInput.value
-    // );
-    books.push('shakes','book')
-
-//    createBooksElements('shakes','newBook') ;
-//    titleInput.value = " ";
-//    nameInput.value= " ";
+    addBook(new Book(title, nam))
 };
    
 
